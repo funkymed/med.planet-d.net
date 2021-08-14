da4e89374f85b4831e8d9c471cb33809bef8219f
Cyril Pereira <cyril.pereira@gmail.com>
2021-08-14 22:30:21 +0200

    Added multiple rasters

--- src/Components/Rasters.js
+++ src/Components/Rasters.js
@@ -4,15 +4,12 @@ export default class Rasters {
   TMPrasterCTX;
   ctx;
   colorRaster = [
-    "#5555FF",
     "#DD5555",
+    "#5555FF",
     "#55AA55",
-    /*"violet",
-    "orange",
-    "lime",
-    "gray",
-    "#ffbb00",
-    "yellow",*/
+    "#FF55FF",
+    "#FF9955",
+    "#5599FF",
   ];
   rasters = [];
   order = 0;
@@ -48,7 +45,7 @@ export default class Rasters {
   }
 
   animate(time) {
-    this.phase = time / 400;
+    this.phase = time / 500;
     this.TMPrasterCTX.clearRect(0, 0, this.cnv.width, this.cnv.height);
     const center = (Math.sin(this.phase / 2) * this.cnv.height) / 4;
 
@@ -56,9 +53,8 @@ export default class Rasters {
       const raster = this.rasters[x];
 
       const posY =
-        this.cnv.height / 2 + Math.sin(this.phase + raster.order * 2) * 30;
-      
-      if (posY <= 353) {
+        this.cnv.height / 2 + Math.sin(this.phase + raster.order * this.rasters.length * 4) * 60;
+      if (posY <= 351) {
         raster.zindex = 2;
       } else if (posY >= 401) {
         raster.zindex = 1;
