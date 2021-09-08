e820d073e3a04c73e5641dd926ca1caabcf079b9
Cyril Pereira <cyril.pereira@gmail.com>
2021-09-08 13:53:39 +0200

    fixed about legend

--- src/tools/const.js
+++ src/tools/const.js
@@ -3,5 +3,5 @@ export const TITLE_CHIPTUNE = "Chiptune track";
 export const TITLE_FIRST = "First place at a Demoparty";
 export const TITLE_SECOND = "Second place at a Demoparty";
 export const TITLE_THIRD = "Third place at Demoparty";
-export const TITLE_BEST = "Personnal favorite track";
-export const TITLE_LOVE = "Technical track";
+export const TITLE_BEST = "Technical track";
+export const TITLE_LOVE = "Personnal favorite track"
